6938aaff4ee70c3bf23bd6d70574dd8906e8cc69
rana <ranasobeid95@gmail.com>
2019-07-24 09:12:18 +0300

    handel error for xhr response , rename script to request Relates #15

---
+++ public/js/request.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('.input-text');
+
+const makeRequest = (url, render,handleError) => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 ) {
+            if(xhr.status !== 200){
+                return handleError(xhr.statusText);
+            }
+
+            if(xhr.getResponseHeader('Content-type').includes('json')){
+                return render(JSON.parse(xhr.responseText));
+            } handleError('Error not Json') ;   
+        }
+    } 
+        xhr.open('GET',url);
+         xhr.send();
+} ;
+  
+
+const handleError = (error) => {
+    const errorSpan = document.createElement('span');
+    errorSpan.textContent = error || 'Erorr response not completed';
+    document.querySelector('#container').appendChild(errorSpan);
+    
+}
