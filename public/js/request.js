fb00fed5ad98e4d34eac6ff4b4586e0305103d7e
rana <ranasobeid95@gmail.com>
2019-07-24 13:20:50 +0300

    populate dom to show all country suggestions Relates #22

--- public/js/request.js
+++ public/js/request.js
@@ -1,9 +1,16 @@
 const input = document.querySelector('#input');
-
+const form =document.querySelector('#search-button');
+const divList =document.querySelector('#list')
+const submit = document.querySelector('#submit');
+const creatElement = (ele) => {
+    return document.createElement(ele);
+}
+const appendElement = (parent,child) => {
+    return parent.appendChild(child);
+}
 const makeRequest = (url, render,handleError) => {
     let xhr = new XMLHttpRequest();
-    console.log(xhr);
-    console.log(url)
+
     xhr.onreadystatechange = () => {
         if(xhr.readyState === 4 ) {
             if(xhr.status !== 200){
@@ -12,13 +19,29 @@ const makeRequest = (url, render,handleError) => {
             if(xhr.getResponseHeader('Content-type').includes('json')){
                 let responseData = JSON.parse(xhr.responseText);
                 const countryName =[];
+                if(!input.value){
+                    divList.textContent = '';
+                    return ;
+                }
                 responseData.forEach((element)=> {
                     if(element.name.toUpperCase().search(input.value.toUpperCase())===0){
                         countryName.push (element.name); 
+                       
                     }
                 });
-                console.log(countryName, '************');
-                return render(countryName[0]);
+               
+                countryName.forEach(element => {
+                    
+                    const cuntryList = creatElement("ul");
+                    const cuntryItem = creatElement("li");
+                    cuntryList.style.listStyleType = 'none';
+                    cuntryItem.textContent = element ;
+                    appendElement(cuntryList,cuntryItem);
+                    appendElement(divList , cuntryList);
+                   
+
+                });
+                return render(responseData, countryName[0]);
 
             } handleError('Error not Json') ;   
         }
@@ -34,9 +57,24 @@ const handleError = (error) => {
     document.querySelector('#container').appendChild(errorSpan);
     
 }
-const render = (result)=>{
+const render = (result, item)=>{
+    // result.forEach(ele=> {
+    //     if(ele.name === item){
+    //         const countryName1 = creatElement('p');
+    //         const countryId = creatElement('p');
+    //         countryName1.textContent = ele.name;
+    //         countryId.textContent = ele.code;
+    //         appendElement(form, countryId);
+    //         appendElement(form, countryName1);
+    //     }
+    // })
 }
 input.addEventListener('input' ,(e)=>{
+    divList.textContent = '';
     makeRequest("http://localhost:3000/data",render,handleError);
 
 });
+// submit.addEventListener('click', (e)=> {
+//     render(result, item);
+    
+// })
