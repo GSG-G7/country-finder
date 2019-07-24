553a3291113d5808c8c600685876d9488ac27341
rana <ranasobeid95@gmail.com>
2019-07-24 16:23:31 +0300

    added process env port to server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const router = require('./router');
 
 
 const server = http.createServer(router);
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server is up to localhost: ${port}`);
 });
